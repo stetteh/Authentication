315ad390a52f506e1e826a272039f45434770a43
stetteh <stettehq@gmail.com>
2016-04-19 14:24:48 -0500

    Account User properties and migration

--- Authentication/Models/IdentityModels.cs
+++ Authentication/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -16,6 +17,12 @@ namespace Authentication.Models
             // Add custom user claims here
             return userIdentity;
         }
+              
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string NickName { get; set; }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
